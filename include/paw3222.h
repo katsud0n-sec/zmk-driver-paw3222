4f2f576af31fecde27f48a2d4712e12fe3d7fc99
katsud0n <miyaokashinpei@icloud.com>
2025-08-21 14:45:05 +0900

    Update paw3222.h

--- include/paw3222.h
+++ include/paw3222.h
@@ -1,28 +1,55 @@
-/*
- * Copyright 2024 Google LLC
- * Modifications Copyright 2025 sekigon-gonnoc
- * Original source code: https://github.com/zephyrproject-rtos/zephyr/blob/19c6240b6865bcb28e1d786d4dcadfb3a02067a0/include/zephyr/input/input_paw32xx.h
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+#pragma once
 
-#ifndef ZEPHYR_INCLUDE_INPUT_PAW32XX_H_
-#define ZEPHYR_INCLUDE_INPUT_PAW32XX_H_
+#include <stdint.h>
+#include <zephyr/drivers/sensor.h>
 
-/**
- * @brief Set resolution on a paw32xx device
- *
- * @param dev paw32xx device.
- * @param res_cpi CPI resolution, 200 to 3200.
- */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Sensor registers (addresses) */
+#define PAW32XX_PRODUCT_ID1 0x00
+#define PAW32XX_PRODUCT_ID2 0x01
+#define PAW32XX_MOTION 0x02
+#define PAW32XX_DELTA_X 0x03
+#define PAW32XX_DELTA_Y 0x04
+#define PAW32XX_OPERATION_MODE 0x05
+#define PAW32XX_CONFIGURATION 0x06
+#define PAW32XX_WRITE_PROTECT 0x09
+#define PAW32XX_SLEEP1 0x0a
+#define PAW32XX_SLEEP2 0x0b
+#define PAW32XX_SLEEP3 0x0c
+#define PAW32XX_CPI_X 0x0d
+#define PAW32XX_CPI_Y 0x0e
+#define PAW32XX_DELTA_XY_HI 0x12
+#define PAW32XX_MOUSE_OPTION 0x19
+
+/* Sensor identification values */
+#define PRODUCT_ID_PAW32XX 0x30
+
+/* SPI command bits */
+#define SPI_WRITE BIT(7)
+
+/* Motion status bits */
+#define MOTION_STATUS_MOTION BIT(7)
+
+/* Operation mode bits */
+#define OPERATION_MODE_SLP_ENH BIT(4)
+#define OPERATION_MODE_SLP2_ENH BIT(3)
+#define OPERATION_MODE_SLP_MASK (OPERATION_MODE_SLP_ENH | OPERATION_MODE_SLP2_ENH)
+
+/* Configuration bits */
+#define CONFIGURATION_PD_ENH BIT(3)
+#define CONFIGURATION_RESET BIT(7)
+
+/* Write protect commands */
+#define WRITE_PROTECT_ENABLE 0x00
+#define WRITE_PROTECT_DISABLE 0x5a
+
+/* Public functions */
 int paw32xx_set_resolution(const struct device *dev, uint16_t res_cpi);
-
-/**
- * @brief Set force awake mode on a paw32xx device
- *
- * @param dev paw32xx device.
- * @param enable whether to enable or disable force awake mode.
- */
 int paw32xx_force_awake(const struct device *dev, bool enable);
 
-#endif /* ZEPHYR_INCLUDE_INPUT_PAW32XX_H_ */
+#ifdef __cplusplus
+}
+#endif
